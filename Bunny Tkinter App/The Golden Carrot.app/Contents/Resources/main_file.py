4b452e487612026ea801f1aec790b4859f16a277
Valerie <valerielamm.com@gmail.com>
2018-10-29 19:04:48 -0500

    Close drawing window on save
    Closes drawing window on save and a couple new stages...

--- Bunny Tkinter App/The Golden Carrot.app/Contents/Resources/main_file.py
+++ Bunny Tkinter App/The Golden Carrot.app/Contents/Resources/main_file.py
@@ -15,6 +15,8 @@ root = Tk()
 import PIL
 from PIL import Image, ImageDraw, ImageTk
 
+
+# Imported module functions
 from start import *
 
 
@@ -25,20 +27,34 @@ class GuessingGame:
         # Title of page
         master.title("The Golden Carrot")
 
+        # Main Text
         self.message = "The Golden Carrot"
         self.label_text = StringVar()
         self.label_text.set(self.message)
         self.label = Label(master, textvariable=self.label_text)
+        self.label.grid(row=0, column=0, columnspan=2, sticky=W + E)
+        self.label.config(background="#d8dad9", height=20, width=60)
 
+        # Bunny Name
         self.b_name = "Bunny"
         self.label_bun = StringVar()
         self.label_bun.set(self.b_name)
         self.bun = Label(master, textvariable=self.label_bun)
 
+
+        # Main 2 Buttons
         self.yes_button = Button(master, text="Continue", command=self.introductions)
         self.no_button = Button(master, text="Back", state=DISABLED)
         self.name = Entry(master)
         self.name.config(highlightbackground="#d8dad9", highlightcolor="#d8dad9")
+        self.no_button.grid(row=2, column=0, sticky=W + E)
+        self.no_button.config(highlightbackground="#d8dad9", highlightcolor="#d8dad9")
+        self.yes_button.grid(row=2, column=1, sticky=W + E)
+        self.yes_button.config(highlightbackground="#d8dad9", highlightcolor="#d8dad9")
+
+        # Options
+        self.option_one = True
+        self.option_two = True
 
         # Drawing
         self.top = Toplevel()
@@ -51,14 +67,7 @@ class GuessingGame:
         self.drawing_setup()
         self.top.withdraw()
 
-
-        self.label.grid(row=0, column=0, columnspan=2, sticky=W+E)
-        self.label.config(background="#d8dad9", height=20, width=60)
-        self.no_button.grid(row=2, column=0, sticky=W+E)
-        self.no_button.config(highlightbackground="#d8dad9", highlightcolor="#d8dad9")
-        self.yes_button.grid(row=2, column=1, sticky=W+E)
-        self.yes_button.config(highlightbackground="#d8dad9", highlightcolor="#d8dad9")
-
+    # Save Drawing
     def use_save(self, *args):
         self.filename = 'drawing.png'
         self.image1.save(self.filename)
@@ -66,33 +75,9 @@ class GuessingGame:
         self.image3 = Label(root, image=self.image2)
         self.image3.config(background="#d8dad9")
         self.image3.grid_remove()
+        self.top.withdraw()
 
-    def drawing_setup(self):
-        self.old_x = None
-        self.old_y = None
-        self.line_width = 15
-        self.color = 'black'
-        self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
-
-    def paint(self, event):
-        self.line_width = 15
-        paint_color = self.color
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                               width=self.line_width, fill=paint_color,
-                               capstyle='round')
-            #  --- PIL
-            draw = ImageDraw.Draw(self.image1)
-            draw.line((self.old_x, self.old_y, event.x, event.y), width=5, fill='black')
-
-        self.old_x = event.x
-        self.old_y = event.y
-
-    def reset(self, event):
-        self.old_x, self.old_y = None, None
-
-    # Note: Imported module functions need to be set as an attribute INSIDE the class that uses it
+    # Imported module functions
     introductions = introductions
     question_one = question_one
     question_one_yes = question_one_yes
@@ -102,7 +87,11 @@ class GuessingGame:
     create_window = create_window
     stage_two = stage_two
     stage_three = stage_three
-
+    drawing_setup = drawing_setup
+    paint = paint
+    reset = reset
+    stage_three_fail = stage_three_fail
+    stage_four = stage_four
 
 my_gui = GuessingGame(root)
 root.config(background="#f0f5f5", padx=20, pady=20)
