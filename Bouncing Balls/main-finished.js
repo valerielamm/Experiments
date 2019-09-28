1481f49eee06e68184af88b5908a54106097bb9d
Valerie <valerielamm.com@gmail.com>
2019-09-28 16:01:15 -0500

    Bringing the evil circle into the program
    Now we've defined the evil circle, we need to actually make it appear in our scene. To do this, you need to make some changes to the loop() function.
    
    First of all, create a new evil circle object instance (specifying the necessary parameters), then call its setControls() method. You only need to do these two things once, not on every iteration of the loop.
    At the point where you loop through every ball and call the draw(), update(), and collisionDetect() functions for each one, make it so that these functions are only called if the current ball exists.
    Call the evil ball instance's draw(), checkBounds(), and collisionDetect() methods on every iteration of the loop.

--- Bouncing Balls/main-finished.js
+++ Bouncing Balls/main-finished.js
@@ -172,15 +172,24 @@ while(balls.length < 25) {
 
 // define loop that keeps drawing the scene constantly
 
+var evil = new EvilCircle(random(0,width), random(0,height), true);
+evil.setControls();
+
 function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
   ctx.fillRect(0,0,width,height);
 
   for(var i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if (balls[i].exists){
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect(); 
+    }
   }
+    
+  evil.draw();
+  evil.checkBounds();
+  evil.collisionDetect();
 
   requestAnimationFrame(loop);
 }
