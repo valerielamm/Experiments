e0c5a1505b2ca10f92bef5f383a4566d5a7bc357
Valerie <valerielamm.com@gmail.com>
2019-11-29 18:50:50 -0600

    Two different methods to do a loading bar

--- Loading Bar/functions.js
+++ Loading Bar/functions.js
@@ -1,2 +1,23 @@
-// Loading Bar js
+"use strict";
 
+// Loading Bar One
+function fillBar(seconds) {
+  var bar = document.querySelector('.bar');
+  bar.style.transition = seconds + 's linear width';
+  bar.style.width = '100%';
+} 
+
+// Loading Bar Two
+function fillBarTwo(seconds) {
+  var bar = document.querySelector('.bar-two');
+  var atPercent = 0;
+  var interval = setInterval(function () {
+    atPercent++;
+    bar.style.width = atPercent + '%';
+    console.log('running at', atPercent);
+
+    if (atPercent >= 100) {
+      clearInterval(interval);
+    }
+  }, seconds * 1000 / 100);
+}
